a65ed9f62afb30e50b52c519e5813be020c37ff1
dawa93 <dawa93@naver.com>
2020-11-17 14:56:36 +0900

    refactoring express

--- server/express.js
+++ server/express.js
@@ -2,20 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
+// const { getEndpoints } = require("express-list-endpoints");
 
 const app = express();
 
 let data = { results: [] };
 
 app.use(cors());
+app
+  .route("/messages")
+  .get((req, res) => {
+    res.send(JSON.stringify(data));
+  })
+  .post(jsonParser, (req, res) => {
+    data.results.push(req.body);
+    res.status(201).send(JSON.stringify(req.body));
+  });
 
-app.get("/messages", (req, res) => {
-  res.send(JSON.stringify(data));
-});
+// app.use(cors());
 
-app.post("/messages", jsonParser, function (req, res) {
-  data.results.push(req.body);
-  res.status(201).send(JSON.stringify(req.body));
-});
+// app.get("/messages", (req, res) => {
+//   res.send(JSON.stringify(data));
+// });
+
+// app.post("/messages", jsonParser, function (req, res) {
+//   data.results.push(req.body);
+//   res.status(201).send(JSON.stringify(req.body));
+// });
 
 module.exports = app;
